bac432015ec44d00bf4e013e9f959806242cbec4
kazuya0529 <sabotenchocobo@gmail.com>
2025-03-07 16:38:14 +0900

    正規表現の記述を正しく変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[一-龠々ぁ-んァ-ヴー]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[一-龠々ぁ-んァ-ヴー]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/ }
   validates :birth_date, presence: true
