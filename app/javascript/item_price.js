390efa195b5b6e69cf3d63cd262bf0680a16b89b
kazuya0529 <sabotenchocobo@gmail.com>
2025-03-11 17:59:51 +0900

    取得した要素をそれぞれ変数に格納

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,7 @@
 window.addEventListener('turbo:load', () => {
   console.log("OK");
 });
+
+const priceInput = document.getElementById("item-price");
+const addtaxInput = document.getElementById("add-tax-price")
+const profitInput = document.getElementById("profit")
