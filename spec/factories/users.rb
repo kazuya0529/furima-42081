102ca6bba0de69751c5af519f14080e336e4472b
kazuya0529 <sabotenchocobo@gmail.com>
2025-03-11 19:31:28 +0900

    Rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.email }
-    password              { '1a' + Faker::Internet.password(min_length: 4) }
+    password              { "1a#{Faker::Internet.password(min_length: 4)}" }
     password_confirmation { password }
     last_name             { Faker::Name.last_name }
     first_name            { Faker::Name.first_name }
