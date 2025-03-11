e9d0a7cf1ce69fc2bf65526dad51c63b481df561
kazuya0529 <sabotenchocobo@gmail.com>
2025-03-11 12:28:42 +0900

    バリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,13 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_fee_status
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :scheduled_delivery
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id,
+            numericality: { other_than: 1, message: 'を選択してください' }
 end
