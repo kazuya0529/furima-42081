6afd534480423061841fd7b2c225ecb90a6ebeaf
kazuya0529 <sabotenchocobo@gmail.com>
2025-03-15 18:34:26 +0900

    2つのマイグレーションファイルを編集

--- db/migrate/20250315085927_create_orders.rb
+++ db/migrate/20250315085927_create_orders.rb
@@ -1,6 +1,13 @@
 class CreateOrders < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
+      t.string     :postal_code,   null: false
+      t.integer    :prefecture_id,    null: false
+      t.string     :city,          null: false
+      t.string     :addresses,     null: false
+      t.string     :building
+      t.string     :phone_number,  null: false
+      t.references :purchase,      null: false, foreign_key: true
 
       t.timestamps
     end
