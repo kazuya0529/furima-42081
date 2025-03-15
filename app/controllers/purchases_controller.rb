19263d10a5199caed267bf5f1ee875874cae913c
kazuya0529 <sabotenchocobo@gmail.com>
2025-03-15 20:06:56 +0900

    送られてきたパラメーターを保存する処理

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,4 +2,14 @@ class PurchasesController < ApplicationController
   def index
     @purchase_order = PurchaseOrder.new
   end
+
+  def create
+    @purchase_order = PurchaseOrder.new(purchase_params)
+    if @purchase_order.valid?
+      @purchase_order.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
 end
