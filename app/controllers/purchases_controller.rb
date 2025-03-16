c2572ab41b0e227fcbb61d771bab91480cf86256
kazuya0529 <sabotenchocobo@gmail.com>
2025-03-16 20:25:26 +0900

    クレジットカード決済機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,15 +1,19 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @purchase_order = PurchaseOrder.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @purchase_order = PurchaseOrder.new(purchase_params)
     if @purchase_order.valid?
+      pay_item
       @purchase_order.save
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
@@ -17,7 +21,20 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_order).permit(:postal_code, :prefecture_id, :city, :addresses, :building,
-                                           :phone_number).merge(user_id: current_user.id)
+    params.require(:purchase_order).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+          .merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
