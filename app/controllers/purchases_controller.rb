607b58978e85fe95933ad47264338f05475e24c0
kazuya0529 <sabotenchocobo@gmail.com>
2025-03-16 21:04:37 +0900

    ログイン状態の場合でも、自身が出品した商品の商品購入ページに遷移しようとすると、商品の販売状況に関わらずトップページに遷移するように記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,6 +2,7 @@ class PurchasesController < ApplicationController
   before_action :set_item, only: [:index, :create]
   before_action :authenticate_user!
   before_action :redirect_if_sold_out, only: [:index, :create]
+  before_action :redirect_if_seller, only: [:index, :create]
 
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
@@ -42,6 +43,12 @@ class PurchasesController < ApplicationController
 
   def redirect_if_sold_out
     return unless @item.purchase.present? && @item.user_id != current_user.id
+
+    redirect_to root_path
+  end
+
+  def redirect_if_seller
+    return unless @item.user_id == current_user.id
 
     redirect_to root_path
   end
