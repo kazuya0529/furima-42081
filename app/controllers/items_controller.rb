f9dfc401c1483af41ad901d9e7a51acf490736cf
kazuya0529 <sabotenchocobo@gmail.com>
2025-03-16 21:35:55 +0900

    ログイン状態の場合でも、自身が出品した売却済み商品の商品情報編集ページへ遷移しようとすると、トップページに遷移するように記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :destroy]
+  before_action :redirect_if_sold_out, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -54,5 +55,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def redirect_if_sold_out
+    redirect_to root_path if @item.purchase.present? && @item.user_id == current_user.id
   end
 end
